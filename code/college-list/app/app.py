2a92eca642a6123be81a421f90a5eb6ba492224d
timholds <tholdsworth@sandiego.edu>
2017-11-17 04:50:45 -0800

    Got hello world working

--- code/college-list/app/app.py
+++ code/college-list/app/app.py
@@ -1,9 +1,10 @@
 import os
 import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-     render_template, flash
+#from flask import Flask
+#import flask
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
-#app = Flask(__name__) # create the application instance :)
+app = Flask(__name__) # create the application instance :)
 #app.config.from_object(__name__) # load config from this file , flaskr.py
 
 # Load default config and override config from an environment variable
@@ -13,6 +14,7 @@ app.config.update(dict(
     USERNAME='admin',
     PASSWORD='default'
 ))
+print(app.root_path)
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 def connect_db():
@@ -20,18 +22,6 @@ def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    """Initializes the database."""
-    init_db()
-    print('Initialized the database.')
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -41,8 +31,87 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+def init_db():
+    """Creates the database tables."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
+
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/')
+def hello_world():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+    #return "Hello, world!"
+
+@app.route('/index')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('templates/show_entries.html', entries=entries)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
+if __name__ == '__main__':
+    init_db()
+    app.run()
+
+
+@app.route('/UCLA')
+def ucla():
+    #TODO make UCLA.html
+    return render_template('UCLA.html')
+
+@app.route('/Berkeley')
+def berkeley():
+    return render_template('Berkeley.html')
+
--- code/college-list/app/views.py
+++ code/college-list/app/views.py
@@ -2,18 +2,24 @@
 from flask import render_template
 from app import app
 
+"""
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template("index.html")
+"""
 
+@app.route('/')
+def nomad_homepage():
+    return render_template('templates/index.html')
 
-@app.route('//')
+@app.route('/')
+@app.route('/index')
 def show_entries():
     db = get_db()
     cur = db.execute('select title, text from entries order by id desc')
     entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
+    return render_template('templates/show_entries.html', entries=entries)
 
 @app.route('/add', methods=['POST'])
 def add_entry():
@@ -26,8 +32,35 @@ def add_entry():
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('show_entries'))
+
+@app.route('/UCLA')
+def ucla():
+    #TODO make UCLA.html
+    return render_template('UCLA.html')
+
+@app.route('/Berkeley')
+def berkeley():
+    return render_template('Berkeley.html')
+
+@app.route('/Irvine')
+def irvine():
+    return render_template('Irvine.html')
