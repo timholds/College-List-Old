aa3f4f79534a84aae08ebfce46ad4ff4e7b97552
timholds <tholdsworth@sandiego.edu>
2017-11-17 03:03:40 -0800

    Adding microblog code

--- code/college-list/app/views.py
+++ code/college-list/app/views.py
@@ -6,3 +6,28 @@ from app import app
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+
+@app.route('//')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, text from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                 [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
